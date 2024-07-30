cf4039bcab8a6f7737b7409d378c2eb9faba0b26
sjieys <0wonyounglee0@gmail.com>
2024-07-30 15:17:14 +0900

    modified

--- app/src/main/java/com/example/timepalette/MainActivity.kt
+++ app/src/main/java/com/example/timepalette/MainActivity.kt
@@ -16,6 +16,6 @@ import com.example.timepalette.ui.theme.TimePaletteTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.calenderlayout)
+        setContentView(R.layout.note)
     }
 }
